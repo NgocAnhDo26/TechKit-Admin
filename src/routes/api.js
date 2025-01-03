d9d2e5a0d7d01d11a9073d9c8b1060571352d779
X13 <hphuc9713@gmail.com>
2025-01-03 16:49:03 +0700

    Now api.js has categoryController in api.js
    I forgot to add last time

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,10 @@
 import accountController from '../components/account/accountController.js';
+import categoryController from '../components/category/categoryController.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.use('/account',accountController);
+router.use('/category',categoryController);
 
 export default router;
